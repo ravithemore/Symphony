3ec6b9340fb909870e517784f2a02e4b68b2bdaf
Ravi M Damor <damorravi540@gmail.com>
2026-01-03 00:03:16 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Webapp from './Webapp'
+import Hero from './Hero'
+
+const App = () => {
+  return (
+    <div>
+      <Hero/>
+    </div>
+  )
+}
+
+export default App
